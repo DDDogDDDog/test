e332957b5928fd5ac4c9a806de1be4108b9292da
dddog <ddddddog@aliyun.com>
2016-05-27 20:53:04 +0800

    print

--- 受到/受到/ViewController.swift
+++ 受到/受到/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("haha");
         print("wudu");
+        print("dd");
+        print("");
     }
 
     override func didReceiveMemoryWarning() {
